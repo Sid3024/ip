59e61a21718142aa201bcd4f7d5c320f0839878a
Sid3024 <siddharth.arum@gmail.com>
2026-02-02 17:49:28 +0800

    Refactor Duke->Sid; add greeting & exit

---
+++ src/main/java/Sid.java
@@ -0,0 +1,11 @@
+public class Sid {
+    public static void main(String[] args) {
+        String logo = "______________________________\n"
+                + "Hello! I'm Sid\n"
+                + "What can I do for you?\n"
+                + "______________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "______________________________\n";
+        System.out.println(logo);
+    }
+}
