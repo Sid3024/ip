c209e993038aeaebbf846f4d327e4c3450df6c3c
Sid3024 <siddharth.arum@gmail.com>
2026-02-02 18:17:48 +0800

    implemented echo()

--- src/main/java/Sid.java
+++ src/main/java/Sid.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Sid {
 
     public static void main(String[] args) {
         Sid sid = new Sid();
+        Scanner scanner = new Scanner(System.in);
+        String user_input;
         sid.hello();
+        while (true) {
+            user_input = scanner.nextLine();
+            if (user_input.equals("bye")) {
+                break;
+            } else {
+                sid.echo(user_input);
+            }
+        }
         sid.bye();
     }
 
-    
+    /**
+     * Prints hello message
+     */
     public void hello() {
         System.out.println("______________________________\n"
                 + "Hello! I'm Sid\n"
@@ -14,9 +28,22 @@ public class Sid {
                 + "______________________________\n");
     }
 
+    /**
+     * Prints bye message
+     */
     public void bye() {
         System.out.println("______________________________\n" +
                 " Bye. Hope to see you again soon!\n"
+                + "______________________________\n");
+    }
+
+    /**
+     * Echoes user input
+     */
+    public void echo(String s) {
+        System.out.println("______________________________\n"
+                + s
+                + "\n"
                 + "______________________________\n");
     }
 
